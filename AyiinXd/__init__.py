05ff90a72ccea8f91b293c14b4b2f63197d397e9
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-04-16 08:09:16 +0800

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -1057,6 +1057,7 @@ with bot:
             if event.query.user_id == uid or event.query.user_id in SUDO_USERS:
                 languages = get_languages()
                 language[0] = languages
+                set_key(event, languages, languange)
                 tgbot.del_key("language") if languages == "id" else tgbot.set_key("language", languages)
                 await event.edit(
                     f"Your language has been set to {languages[languages]['natively']} [{languages}].",
@@ -1075,6 +1076,7 @@ with bot:
             if event.query.user_id == uid or event.query.user_id in SUDO_USERS:
                 languages = get_languages()
                 language[0] = languages
+                set_key(event, languages, languange)
                 tgbot.del_key("language") if languages == "en" else tgbot.set_key("language", languages)
                 await event.edit(
                     f"Your language has been set to {languages[languages]['natively']} [{languages}].",
