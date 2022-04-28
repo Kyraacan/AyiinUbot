33a22fb32e2f471a305c20ed1b87122d2f9db717
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-04-29 03:05:14 +0800

    Update stringsyins.py

--- AyiinXd/modules/stringsyins.py
+++ AyiinXd/modules/stringsyins.py
@@ -8,19 +8,19 @@ import os
 from telethon import Button, custom, events
 
 from AyiinXd import CMD_HANDLER as cmd
-from AyiinXd import CMD_HELP, INLINE_PIC
+from AyiinXd import CMD_HELP, INLINE_PIC, bot
 from AyiinXd.ayiin import ayiin_cmd, eod, eor
 from Stringyins import get_string
 from .button import BTN_URL_REGEX, build_keyboard
 
-@ayiin_cmd(pattern="string(?:\\s|$)([\\s\\S]*)")
+@bot.on(ayiin_cmd(pattern="string(?:\\s|$)([\\s\\S]*)"))
 async def test_string(stringyins):
     ayiin = await eor(stringyins, get_string("com_1"))
     text = "Silahkan klik Dibawah Ini Untuk Membuat String Anda"
     buttons = [
         [
-           Button.url("String Via Bot", "https://t.me/AyiinStringRobot"),
-           Button.url("String Via Web", "https://repl.it/@AyiinXd/AyiinString?lite=1&outputonly=1"),
+           (custom.Button.url("String Via Bot", "https://t.me/AyiinStringRobot"),),
+           (custom.Button.url("String Via Web", "https://repl.it/@AyiinXd/AyiinString?lite=1&outputonly=1"),),
         ]
     ]
     if INLINE_PIC:
