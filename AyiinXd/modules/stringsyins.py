6cf254ca3ecb8bf69845299db71b90630a69fb59
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-04-29 13:16:33 +0800

    Update stringsyins.py

--- AyiinXd/modules/stringsyins.py
+++ AyiinXd/modules/stringsyins.py
@@ -13,6 +13,35 @@ from AyiinXd.ayiin import ayiin_cmd, eod, eor, reply_id
 from Stringyins import get_string
 from .button import BTN_URL_REGEX, build_keyboard
 
+
+async def inline_query(
+        self: 'TelegramClient',
+        bot: 'hints.DialogLike',
+        query: str,
+        *,
+        dialog: 'hints.DialogLike' = None,
+        offset: str = None,
+        geo_point: '_tl.GeoPoint' = None) -> _custom.InlineResults:
+    bot = await self._get_input_peer(bot)
+    if dialog:
+        peer = await self._get_input_peer(dialog)
+    else:
+        peer = _tl.InputPeerEmpty()
+
+    try:
+        result = await self(_tl.fn.messages.GetInlineBotResults(
+            bot=bot,
+            peer=peer,
+            query=query,
+            offset=offset or '',
+            geo_point=geo_point
+        ))
+    except errors.BotResponseTimeoutError:
+        raise asyncio.TimeoutError from None
+
+    return _custom.InlineResults(self, result, entity=peer if dialog else None)
+
+
 @ayiin_cmd(pattern="string(?:\\s|$)([\\s\\S]*)")
 async def test_string(event):
     ayiin = await eor(event, get_string("com_1"))
