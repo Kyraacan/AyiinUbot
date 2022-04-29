161596adb85e5890582615078fca3cc70ad32ce0
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-04-29 14:25:29 +0800

    Update stringsyins.py

--- AyiinXd/modules/stringsyins.py
+++ AyiinXd/modules/stringsyins.py
@@ -14,30 +14,35 @@ from Stringyins import get_string
 from .button import BTN_URL_REGEX, build_keyboard
 
 
+Y_BUTTONS = [
+        [
+           custom.Button.url("String Via Bot", "https://t.me/AyiinStringRobot"),
+           custom.Button.url("String Via Web", "https://repl.it/@AyiinXd/AyiinString?lite=1&outputonly=1"),
+        ]
+    ]
+
+
 @ayiin_cmd(pattern="string(?:\\s|$)([\\s\\S]*)")
 async def test_string(event):
     ayiin = await eor(event, get_string("com_1"))
-    reply_to_id = await reply_id(event)
-    reply_message = await event.get_reply_message()
-    if reply_message:
-        markdown_note = reply_message.text
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in Y_BUTTONS:
+            await eor(event, f"Silahkan klik Dibawah Ini Untuk Membuat String Anda\n\n {Y_BUTTONS[args]}")
+        else:
+            await eod(event, "Harap Msukkan Yg Benar")
     else:
-        text = "Silahkan klik Dibawah Ini Untuk Membuat String Anda"
-        markdown_note = "".join(event.text.split(maxsplit=1)[1:])
-    if not markdown_note:
-        return await eod(event, f"**Gunakan {cmd}string yins**"
-        )
-    buttons = [
-        [
-           Button.url("String Via Bot", "https://t.me/AyiinStringRobot"),
-           Button.url("String Via Web", "https://repl.it/@AyiinXd/AyiinString?lite=1&outputonly=1"),
-        ]
-    ]
-    yinsbut = "Silahkan klik Dibawah Ini Untuk Membuat String Anda" + markdown_note
-    results = await event.client.inline_query(BOT_USERNAME, "@AyiinXdSupport")
-    await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
-    await event.delete()
-
+        try:
+            results = await event.client.inline_query(  # pylint:disable=E0602
+                BOT_USERNAME, "@AyiinXdSupport",
+            )
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        except Exception as e:
+            await eor(event, get_string("error_1").format(e)
+                      )
 
 CMD_HELP.update(
   {
