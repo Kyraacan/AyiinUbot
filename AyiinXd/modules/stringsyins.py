4339ca1980b231fdd3fc45cb10d63f32bb80315f
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-04-29 13:00:20 +0800

    Update stringsyins.py

--- AyiinXd/modules/stringsyins.py
+++ AyiinXd/modules/stringsyins.py
@@ -17,15 +17,14 @@ from .button import BTN_URL_REGEX, build_keyboard
 async def test_string(event):
     ayiin = await eor(event, get_string("com_1"))
     reply_to_id = await reply_id(event)
-    text = "Silahkan klik Dibawah Ini Untuk Membuat String Anda"
+    buttons = "Silahkan klik Dibawah Ini Untuk Membuat String Anda"
     buttons = [
         [
            Button.url("String Via Bot", "https://t.me/AyiinStringRobot"),
            Button.url("String Via Web", "https://repl.it/@AyiinXd/AyiinString?lite=1&outputonly=1"),
         ]
     ]
-    catinput = "Silahkan klik Dibawah Ini Untuk Membuat String Anda " + buttons
-    results = await event.client.inline_query(BOT_USERNAME, catinput)
+    results = await event.client.inline_query(BOT_USERNAME, buttons)
     await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
     await event.delete()
 
