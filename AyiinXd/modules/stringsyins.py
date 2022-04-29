e746e286dbb322d2c8ce22fa6474886d4537c3d5
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-04-29 13:32:48 +0800

    Update stringsyins.py

--- AyiinXd/modules/stringsyins.py
+++ AyiinXd/modules/stringsyins.py
@@ -8,7 +8,7 @@ import os
 from telethon import Button, custom, events
 
 from AyiinXd import CMD_HANDLER as cmd
-from AyiinXd import CMD_HELP
+from AyiinXd import BOT_USERNAME, CMD_HELP
 from AyiinXd.ayiin import ayiin_cmd, eod, eor, reply_id
 from Stringyins import get_string
 from .button import BTN_URL_REGEX, build_keyboard
@@ -18,14 +18,23 @@ from .button import BTN_URL_REGEX, build_keyboard
 async def test_string(event):
     ayiin = await eor(event, get_string("com_1"))
     reply_to_id = await reply_id(event)
-    text = "Silahkan klik Dibawah Ini Untuk Membuat String Anda"
+    reply_message = await event.get_reply_message()
+    if reply_message:
+        markdown_note = reply_message.text
+    else:
+        text = "Silahkan klik Dibawah Ini Untuk Membuat String Anda"
+        markdown_note = "".join(event.text.split(maxsplit=1)[1:])
+    if not markdown_note:
+        return await eod(event, f"**Gunakan {cmd}string yins**"
+        )
     buttons = [
         [
            Button.url("String Via Bot", "https://t.me/AyiinStringRobot"),
            Button.url("String Via Web", "https://repl.it/@AyiinXd/AyiinString?lite=1&outputonly=1"),
         ]
     ]
-    results = await event.client.inline_query(text, buttons)
+    yinsbut = "Silahkan klik Dibawah Ini Untuk Membuat String Anda" + markdown_note
+    results = await event.client.inline_query(BOT_USERNAME, yinsbut)
     await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
     await event.delete()
 
