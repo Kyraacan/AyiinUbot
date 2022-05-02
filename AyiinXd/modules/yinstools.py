afe66d11f02535f6791b9ad61902cfb76ce4988a
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-05-02 09:33:19 +0800

    Update yinstools.py

--- AyiinXd/modules/yinstools.py
+++ AyiinXd/modules/yinstools.py
@@ -3,15 +3,18 @@
 #
 #
 
+import re
+import os
 
 from pprint import pprint
 from io import BytesIO
+from telethon import Button, custom, events
 
 from AyiinXd import CMD_HANDLER as cmd
-from AyiinXd import CMD_HELP
-from AyiinXd.ayiin import ayiin_cmd, bash
-
+from AyiinXd import BOT_USERNAME, CMD_HELP
+from AyiinXd.ayiin import ayiin_cmd, bash, eod, eor
 from Stringyins import get_string
+
 
 # Used for Formatting Eval Code, if installed
 try:
@@ -20,6 +23,27 @@ except ImportError:
     black = None
 
 p, pp = print, pprint
+
+
+def build_keyboards(buttons):
+    keyb = []
+    for btn in buttons:
+        if btn[0] and keyb:
+            keyb[0].append(Button.url(btn[0], btn[0]))
+        else:
+            keyb.append([Button.url(btn[0], btn[0])])
+    return keyb
+
+
+Y_BUTTONS = [
+        [
+           custom.Button.url("Bᴏᴛ Sᴛʀɪɴɢ", "https://t.me/AyiinStringRobot"),
+           custom.Button.url("Rᴇᴘʟɪᴛ Sᴛʀɪɴɢ", "https://repl.it/@AyiinXd/AyiinString?lite=1&outputonly=1"),
+        ],
+        [
+           custom.Button.url("Sᴜᴘᴘᴏʀᴛ", "https://t.me/AyiinXdSupport"),
+        ],
+    ]
 
 
 @ayiin_cmd(pattern="bash(?: |$)(.*)")
@@ -59,11 +83,32 @@ async def _(event):
         await xx.edit(OUT)
 
 
+@ayiin_cmd(pattern="string(?:\\s|$)([\\s\\S]*)")
+async def test_string(event):
+    ayiin = await eor(event, get_string("com_1"))
+    buttons = build_keyboards(Y_BUTTONS)
+    if buttons:
+        try:
+            results = await event.client.inline_query(  # pylint:disable=E0602
+                BOT_USERNAME, "string",
+            )
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        except Exception as e:
+            await eor(event, get_string("error_1").format(e)
+                      )
+
+
+
 CMD_HELP.update(
     {
         "yinstools": f"**Plugin :** `yinstools`\
         \n\n  •  **Perintah :** `{cmd}bash <command>`\
         \n  •  **Kegunaan : **__Menjalankan perintah linux di telegram.__\
+        \n\n  •  **Perintah :** `{cmd}string`\
+        \n  •  **Kegunaan :** Mendapatkan Bot String dan Web String.\
     "
     }
 )
